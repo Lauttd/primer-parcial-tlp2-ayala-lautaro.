9a643ad50ad6b677abf24fc728a1a229a8f00c98
Lauttd <lautaroayala123090@gmail.com>
2025-09-29 10:57:41 -0300

    se hizo el controller de register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,21 @@
-import { signToken, verifyToken } from "../helpers/jwt.helper.js";
 import { comparePassword } from "../helpers/bcrypt.helper.js";
-import { UserModel } from "../models/mongoose/user.model";
-import { generateToken } from "../../../trabajo-practico-integrador-2/src/helpers/jwt.helper";
+import { UserModel } from "../models/mongoose/user.model.js";
+import { hashPassword } from "../helpers/bcrypt.helper.js";
 
 export const register = async (req, res) => {
+  const { username, email, password, profile } = req.body;
   try {
-    return res.status(201).json({ msg: "Usuario registrado correctamente" });
+    const hashedPassword = await hashPassword(password);
+
+    const newUser = await UserModel.create({
+      username,
+      email,
+      password: hashedPassword,
+      profile,
+    });
+    return res
+      .status(201)
+      .json({ msg: "Usuario registrado correctamente", data: newUser });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ msg: "Error interno del servidor" });
--- src/helpers/jwt.helper.js
+++ src/helpers/jwt.helper.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 // * funcion para crear un token con una firma secreta y un tiempo de expiración
-export const signToken = (payload) =>
+export const signToken = (user) =>
   jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
 
 // * funcion para verificar que un token este con la misma firma secreta
